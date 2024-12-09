7b85efcf71006a156605daa2f812c937209a7274
Naresh <Naresh.21573@sscbs.du.ac.in>
2024-12-09 22:29:11 +0530

    started the project

---
+++ Packages.md
@@ -0,0 +1,11 @@
+### Packages Used
+```
+
+1) npm i antd
+2) npm install @ant-design/icons
+3) npm i axios
+4) npm i zustand
+5) npm i react-router-dom
+6)  npm i react-icons
+
+```
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Button} from 'antd'
+
+function App() {
+
+  return (
+    <>
+      <Button type="primary" ghost>
+        Primary
+      </Button>
+    </>
+  )
+}
+
+export default App
